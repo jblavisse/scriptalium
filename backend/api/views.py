26ad53190b26c387d7b3dcb876dbd3cb7de15762
Ericdsr <eric62.dasilvarocha@laposte.net>
2024-09-17 09:17:52 +0200

    Remove: test entities after successful verification

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,10 +2,3 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET'])
-def hello_world(request):
-    data = {
-        "message": "Hello,World !",
-        "items": ["Article 1", "Article 2", "Article 3"]
-    }
-    return Response(data)
